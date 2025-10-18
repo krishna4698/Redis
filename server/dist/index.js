c89ba92f8d4947b5b319e4d5015312a7e15b6d19
krishna joshi <krishnajoshi4698@gmail.com>
2025-10-18 17:40:46 +0530

    implemented logic

---
+++ server/dist/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { createClient } from 'redis';
+const app = express();
+app.use(express.json());
+const client = createClient();
+client.connect();
+app.post('/submit', async (req, res) => {
+    const { id, language, code } = req.body;
+    try {
+        await client.lPush("submissions", JSON.stringify({ id, language, code }));
+        res.json({
+            message: "submission recieved"
+        });
+    }
+    catch (e) {
+        res.json({
+            e
+        });
+    }
+});
+app.listen(3000);
+//# sourceMappingURL=index.js.map
