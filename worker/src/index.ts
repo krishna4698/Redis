48d364398d3f84fe485dfe08bbdf815585a6cba6
krishna joshi <krishnajoshi4698@gmail.com>
2025-10-18 18:14:33 +0530

    added worker logic

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,1 +1,13 @@
-import 
+import  {createClient} from 'redis'
+
+main();
+ async function main(){
+    const client= createClient();
+    client.connect();
+    while(1){
+        const response= await client.brPop("submissions", 0);
+     console.log(response);
+    console.log("processed user submissions")   
+    }
+       
+}
